daca8ecefe99d04431f1be964c5dfb2346bec9a5
“Евгений <kea-asd@yandex.ru>
2022-05-05 18:06:25 +0700

    ai

--- del/web-pack/webpack.config.js
+++ del/web-pack/webpack.config.js
@@ -1,54 +1,20 @@
 const path = require('path');
 const HtmlWebpackPlugin = require ('html-webpack-plugin');
-const MiniCssExtractPlugin = require ('mini-css-extract-plugin');
 
-const devServer = (isDev) => !isDev ? {} : {
-	devServer: {
-		open: true,
-		hot: true,
-		port: 8081,
-	}
-};
-module.exports = ({develop}) => ({
+module.exports = {
     mode: develop ? 'development' : 'production',
-    entry: './src/index.js',
+    entry:{
+        main: './src/index.js',
+        analytics: './src/analitics.js'
+    },
     output: {
-        path: path.resolve(__dirname, 'dist'),
-        filename: 'bundle.js',
-        clean:  true,
+        filename: '[name].[contanthash].js',
+        path: path.resolve(__dirname, 'dist')
     },
     plugins: [
         new HtmlWebpackPlugin({
+            title: 'Document',
             template: './src/index.html'
-        }),
-        new MiniCssExtractPlugin({
-            filename: './styles/main.css'
         })
-        ],
-        module: {
-            rules: [
-                {
-                    test: /\.(?:ico|png|jpg|jpeg|svg|)$/i,
-                    type: 'asset/inline'
-                },
-                {
-                    test: /\.html$/i,
-                    loader: 'html-loader'
-                },
-                {
-                    test: /\.css$/i,
-                    use: [
-                        MiniCssExtractPlugin.loader, 'css-loader'
-                    ]
-                },
-                {
-                    test: /\.scss$/i,
-                    use: [
-                        MiniCssExtractPlugin.loader, 'css-loader', 'sass-loader'
-                    ]
-                }
-            ]
-        },
-        ...devServer(develop),
-
-});
+        ]
+}
